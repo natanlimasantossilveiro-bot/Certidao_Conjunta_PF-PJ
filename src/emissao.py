02c673b2f4f28fb44c2196e6c82624d5844ba14e
Natan Lima <natanjonatan3103@gmail.com>
2026-04-21 01:38:00 -0300

    Commit teste

--- src/emissao.py
+++ src/emissao.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 def clicar_botao_aceitar_cookies(driver):
         wait = WebDriverWait(driver, 10)
 
